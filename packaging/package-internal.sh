6651b6a4e0a713ec2e43d418cb99c2df43ed8f6e
Koutheir Attouchi <koutheir@gmail.com>
2025-09-09 00:14:56 -0400

    Minor fixes of shellcheck lints.

--- packaging/package-internal.sh
+++ packaging/package-internal.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 set -e
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 # This script generates packages for a release and places them in target/packages/<version>.
 # Don't use it directly, use package.sh instead.
 
-cd "$(dirname $0)/.."
+cd "$(dirname "$0")/.."
 
 echo "${bold}Building${normal}"
 set -x
@@ -18,19 +20,19 @@ set +x
 
 echo "${bold}Packaging${normal} fclones"
 set -x
-VERSION=$(cargo pkgid -p fclones | sed 's/.*#//')
-PKG_DIR=target/packages/fclones-$VERSION
-mkdir -p $PKG_DIR
-rm -f $PKG_DIR/*
+VERSION="$(cargo pkgid -p fclones | sed 's/.*#//')"
+PKG_DIR="target/packages/fclones-${VERSION}"
+mkdir -p "$PKG_DIR"
+rm -f "$PKG_DIR"/*
 
 cargo deb -p fclones --target x86_64-unknown-linux-gnu
-mv target/x86_64-unknown-linux-gnu/debian/*.deb $PKG_DIR
+mv target/x86_64-unknown-linux-gnu/debian/*.deb "$PKG_DIR"
 
-fakeroot alien --to-rpm -c $PKG_DIR/*.deb
-mv *.rpm $PKG_DIR
-fakeroot alien --to-tgz -c $PKG_DIR/*.deb
-mv *.tgz $PKG_DIR/"fclones-$VERSION-linux-glibc-x86_64.tar.gz"
+fakeroot alien --to-rpm -c "$PKG_DIR"/*.deb
+mv ./*.rpm "$PKG_DIR"
+fakeroot alien --to-tgz -c "$PKG_DIR"/*.deb
+mv ./*.tgz "$PKG_DIR/fclones-${VERSION}-linux-glibc-x86_64.tar.gz"
 
-tar -zcvf $PKG_DIR/"fclones-$VERSION-linux-musl-x86_64.tar.gz" target/x86_64-unknown-linux-musl/release/fclones
-tar -zcvf $PKG_DIR/"fclones-$VERSION-linux-musl-i686.tar.gz" target/i686-unknown-linux-musl/release/fclones
-zip -j $PKG_DIR/"fclones-$VERSION-windows-x86_64.zip" target/x86_64-pc-windows-gnu/release/fclones.exe
+tar -zcvf "$PKG_DIR/fclones-${VERSION}-linux-musl-x86_64.tar.gz" target/x86_64-unknown-linux-musl/release/fclones
+tar -zcvf "$PKG_DIR/fclones-${VERSION}-linux-musl-i686.tar.gz" target/i686-unknown-linux-musl/release/fclones
+zip -j "$PKG_DIR/fclones-${VERSION}-windows-x86_64.zip" target/x86_64-pc-windows-gnu/release/fclones.exe
--- packaging/package.sh
+++ packaging/package.sh
@@ -5,10 +5,10 @@
 # Additionally the pulled in docker container includes an older libc, therefore generated packages
 # will be compatible with older Linux distributions.
 
-FCLONES_HOME=$(realpath "$(dirname $0)/..")
-echo $FCLONES_HOME
-IMAGE="pkolaczk/fclones-builder"
+FCLONES_HOME="$(realpath "$(dirname "$0")/..")"
+echo "$FCLONES_HOME"
+IMAGE='pkolaczk/fclones-builder'
 docker run \
     -v "$FCLONES_HOME":/fclones \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
-    -it $IMAGE /fclones/packaging/package-internal.sh
+    -u "$(id -u "$USER"):$(id -g "$USER")" \
+    -it "$IMAGE" /fclones/packaging/package-internal.sh
